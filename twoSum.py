c7f456be7c409b38b2ee5d2d190b04ab900b6d9d
ahmed-boussaleh <bousalehahmed5@gmail.com>
2024-05-07 11:58:42 +0200

    update the time complixity

--- twoSum.py
+++ twoSum.py
@@ -7,3 +7,15 @@ class Solution:
                     return [i,j]
 
 
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = {}
+        for i in range(len(nums)):
+            if(target-nums[i] in indices):
+                return [i,indices[target-nums[i]]]
+            indices[nums[i]] = i
+    
+
+
